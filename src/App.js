8be5b0125eb2dd6d47ed1b003746222cb3711213
Luis Astudillo <leachdeniro@gmail.com>
2021-06-21 12:38:27 -0400

    carga inicial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ¿A qué hora es el partido? - A las 17:00 hrs.
         </p>
         <a
           className="App-link"
